639e52fdc62663f11de1be98cb0250beed24b16e
Johan <johanlindstroom@icloud.com>
2021-05-10 18:46:06 +0200

    firebase register user added
    working on getting in features fast

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -17,6 +17,40 @@ export default function AuthContextProvider({ children }) {
     return unsubscribe;
   });
 
+  const registerUser = (email, password) => {
+    // auth()
+    //   .createUserWithEmailAndPassword(email, password)
+    //   .then(() => {
+    //     console.log("User account created & signed in!");
+    //   })
+    //   .catch((error) => {
+    //     if (error.code === "auth/email-already-in-use") {
+    //       console.log("That email address is already in use!");
+    //     }
+
+    //     if (error.code === "auth/invalid-email") {
+    //       console.log("That email address is invalid!");
+    //     }
+
+    //     console.error(error);
+    //   });
+    try {
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          if (errorCode == "auth/weak-password") {
+            alert("The password is too weak.");
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+        });
+    } catch (error) {}
+  };
+
   const logIn = async (email, password) => {
     console.log("calling log in");
     try {
@@ -45,7 +79,9 @@ export default function AuthContextProvider({ children }) {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoading, user, logIn, signOut }}>
+    <AuthContext.Provider
+      value={{ isLoading, user, logIn, signOut, registerUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,10 +1,115 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useState, useContext } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { AuthContext } from "../context/AuthContext";
 
 export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { registerUser } = useContext(AuthContext);
+  const navigation = useNavigation();
+
+  const registerPressed = () => {
+    console.log("submitted user register");
+    // logIn(email, password);
+    registerUser(email, password);
+  };
+
+  const navigateSignUp = () => {
+    console.log("Sign Up page pressed");
+    navigation.navigate("SignUp");
+  };
+
   return (
-    <View>
-      <Text>Hello Sign up?</Text>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Register User</Text>
+      </View>
+      <View style={{ flex: 1.5, width: "100%", alignItems: "center" }}>
+        <TextInput
+          placeholder='name@email.com'
+          value={email}
+          onChangeText={setEmail}
+          style={styles.inputStyle}
+          returnKeyType={"next"}
+          autoCompleteType={"off"}
+          clearButtonMode={"always"}
+          keyboardAppearance={"dark"}
+        />
+        <TextInput
+          placeholder='Enter password'
+          value={password}
+          onChangeText={setPassword}
+          style={styles.inputStyle}
+          returnKeyType={"done"}
+          autoCompleteType={"off"}
+          clearButtonMode={"always"}
+          keyboardAppearance={"dark"}
+        />
+        <TouchableOpacity
+          style={styles.customButton}
+          onPress={registerPressed}
+          activeOpacity={0.7}
+        >
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            marginTop: 20,
+          }}
+          onPress={navigateSignUp}
+        >
+          <Text>Sign up with email?</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  inputStyle: {
+    padding: 15,
+    backgroundColor: "#DCDCDC",
+    marginBottom: 15,
+    width: 360,
+    borderRadius: 5,
+  },
+
+  header: {
+    color: "#000",
+    fontSize: 42,
+    fontWeight: "700",
+    paddingTop: 230,
+  },
+
+  headerContainer: {
+    flex: 1.2,
+    width: "70%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+  },
+
+  customButton: {
+    width: 360,
+    height: 45,
+    backgroundColor: "#000",
+    justifyContent: "center",
+    borderRadius: 5,
+    marginTop: 10,
+  },
+
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
